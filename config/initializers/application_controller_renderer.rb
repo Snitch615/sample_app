61e9b9ed0090d0af51d53e961e05f43ecaecb36b
Snitch615 <yamasaki.shohei.infratop@gmail.com>
2020-01-04 15:47:09 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '19d412dbc25146b9e1c9b1db3e4bd2107165dfa30f4be338744802dcbfa4df8aabd028fa80b56a924cd6e2d9069a72aa96afa550beb2d5567a39640a3c424054'
